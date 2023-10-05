3e35d21b27eec84c812f8294f8148a14110afb09
Matheus Andrade Uchoa <uchoamatheus22@gmail.com>
2023-10-04 21:35:16 -0300

    adicionando funcionalidades do jogo e da interface

--- src/main.py
+++ src/main.py
@@ -34,3 +34,91 @@ class CampoMinado:
             for y in range(self.colunas):
                 self.botoes[x][y] = tk.Button(self.root, width=3, height=1, command=lambda x=x, y=y: self.revelar_celula(x, y))
                 self.botoes[x][y].grid(row=x, column=y)
+                
+    def revelar_celula(self, x, y):
+        if self.tabuleiro[x][y] == -1:
+            self.game_over()
+        else:
+            vizinhos = self.calcular_vizinhos(x, y)
+            self.botoes[x][y]['text'] = str(vizinhos)
+            self.botoes[x][y]['state'] = 'disabled'
+            # Após revelar uma célula, verificamos se o jogador venceu
+            if self.verificar_vitoria():
+                self.vitoria()
+                
+    def calcular_vizinhos(self, x, y):
+        bomb_count = 0
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if 0 <= x + i < self.linhas and 0 <= y + j < self.colunas:
+                    if self.tabuleiro[x + i][y + j] == -1:
+                        bomb_count += 1
+        return bomb_count
+    
+    def verificar_vitoria(self):
+        # Verifica se todas as células que não são bombas foram reveladas
+        for x in range(self.linhas):
+            for y in range(self.colunas):
+                if self.tabuleiro[x][y] != -1 and self.botoes[x][y]['state'] != 'disabled':
+                    return False
+        return True
+
+    def vitoria(self):
+        messagebox.showinfo("Parabéns!", "Você venceu o jogo!")
+        self.root.destroy()
+        
+    def game_over(self, mostrar_interface=True):
+        for x in range(self.linhas):
+            for y in range(self.colunas):
+                if self.tabuleiro[x][y] == -1:
+                    self.botoes[x][y]['text'] = 'X'
+                self.botoes[x][y]['state'] = 'disabled'
+                
+        if mostrar_interface:
+            user_choice = messagebox.askquestion("Fim de Jogo", "Você perdeu!\nDeseja jogar novamente ou sair?")
+            if user_choice == 'yes':
+                self.root.destroy()  # Fecha a janela atual e voltará para a tela de escolha de níveis
+            else:
+                self.root.destroy()  # Fecha a janela e encerra o jogo
+
+   
+class Jogo:
+    def __init__(self, root):
+        self.root = root
+        self.root.title('Campo Minado')
+        self.mostrar_tela_inicial()
+
+    def mostrar_tela_inicial(self):
+        tk.Label(self.root, text="Escolha o nível de dificuldade:").pack(pady=10)
+
+        tk.Button(self.root, text="Fácil", command=lambda: self.iniciar_jogo(8, 8, 10)).pack(pady=5)
+        tk.Button(self.root, text="Intermediário", command=lambda: self.iniciar_jogo(10, 16, 30)).pack(pady=5)
+        tk.Button(self.root, text="Difícil", command=lambda: self.iniciar_jogo(24, 24, 100)).pack(pady=5)
+
+        tk.Button(self.root, text="Ver Histórico", command=self.ver_historico).pack(pady=10)
+
+    def iniciar_jogo(self, linhas, colunas, bombas):
+        for widget in self.root.winfo_children():
+            widget.destroy()
+
+        self.jogo = CampoMinado(self.root, linhas, colunas, bombas)
+        self.root.geometry(f"{colunas*30}x{linhas*30}")
+        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
+
+    def on_close(self):
+        user_choice = messagebox.askquestion("Sair", "Deseja sair do jogo?")
+        if user_choice == 'yes':
+            self.root.destroy()
+        else:
+            self.mostrar_tela_inicial()
+
+    def ver_historico(self):
+        messagebox.showinfo("Histórico de Resultados", "Aqui está o histórico de resultados:\n\n"
+                                                        "Data e Hora: {}\n"
+                                                        "Nível: {}\n"
+                                                        "Resultado: {}".format(datetime.datetime.now(), "Nível do Jogo", "Resultado do Jogo"))
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    jogo = Jogo(root)
+    root.mainloop()

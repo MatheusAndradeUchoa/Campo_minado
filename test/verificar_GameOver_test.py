a52e11e6f81c182df387d22ca153e471ce6da2db
Matheus Andrade Uchoa <uchoamatheus22@gmail.com>
2023-10-04 21:46:30 -0300

    criando teste para game over

---
+++ test/verificar_GameOver_test.py
@@ -0,0 +1,17 @@
+import pytest
+from main import CampoMinado  
+
+def test_game_over_no_interface():
+    # Criar uma instância de CampoMinado com dimensões 8x8 e 1 bomba
+   
+    campo_minado = CampoMinado(None, 8, 8, 1)
+
+    # Colocar uma bomba em uma célula (0, 0)
+    campo_minado.tabuleiro[0][0] = -1
+
+    # Chamar a função game_over sem exibir a interface
+    campo_minado.game_over(mostrar_interface=False)
+
+    # Verificar se os botões foram desativados e marcados com 'X'
+    assert campo_minado.botoes[0][0]['text'] == 'X'
+    assert campo_minado.botoes[0][0]['state'] == 'disabled'

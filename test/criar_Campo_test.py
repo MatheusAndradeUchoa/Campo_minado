e3a14437af3efabecbc7d31a719a34c95976289e
Matheus Andrade Uchoa <uchoamatheus22@gmail.com>
2023-10-04 21:47:41 -0300

    alterando variaveis pra criar o tabuleiro

--- test/criar_Campo_test.py
+++ test/criar_Campo_test.py
@@ -7,29 +7,23 @@ def test_criar_tabuleiro_facil():
     campo_minado = CampoMinado(None, 8, 8, 10)
     
     # Verifica se o tabuleiro foi criado corretamente
-    assert len(campo_minado.tabuleiro) == linhas
-    assert len(campo_minado.tabuleiro[0]) == colunas
+    assert len(campo_minado.tabuleiro) == 8
+    assert len(campo_minado.tabuleiro[0]) == 8
 
 def test_criar_tabuleiro_intermediario():
-    root = None
-    linhas = 10
-    colunas = 16
-    num_bombas = 30
-    campo_minado = CampoMinado(root, linhas, colunas, num_bombas)
+  
+    campo_minado = CampoMinado(None, 10, 16, 30)
     
     # Verifica se o tabuleiro foi criado corretamente
-    assert len(campo_minado.tabuleiro) == linhas
-    assert len(campo_minado.tabuleiro[0]) == colunas
+    assert len(campo_minado.tabuleiro) == 10
+    assert len(campo_minado.tabuleiro[0]) == 16
     
 
 
 def test_criar_tabuleiro_tamanho_invalido():
     with pytest.raises(ValueError):
-        root = None
-        linhas = 7
-        colunas = 7
-        num_bombas = 10
-        CampoMinado(root, linhas, colunas, num_bombas)
+       
+        CampoMinado(None, 7, 7, 10)
 
 def test_posicoes_bombas():
     
@@ -40,54 +34,12 @@ def test_posicoes_bombas():
     campo_minado.tabuleiro[1][1] = -1
     campo_minado.tabuleiro[2][2] = -1
     campo_minado.tabuleiro[3][3] = -1
-
+    
     assert campo_minado.tabuleiro[0][0] == -1
     assert campo_minado.tabuleiro[1][1] == -1
     assert campo_minado.tabuleiro[2][2] == -1
     assert campo_minado.tabuleiro[3][3] == -1
-    
-def test_verificar_vitoria():
-    # Crie uma instância de CampoMinado com dimensões 4x4 e 4 bombas
-    campo_minado = CampoMinado(None, 8, 8, 4)
-
-    # Defina manualmente as posições das bombas para facilitar os testes
-    # Neste exemplo, colocamos bombas nas células (0, 0), (1, 1), (2, 2) e (3, 3)
-    campo_minado.tabuleiro[0][0] = -1
-    campo_minado.tabuleiro[1][1] = -1
-    campo_minado.tabuleiro[2][2] = -1
-    campo_minado.tabuleiro[3][3] = -1
-
-    # Simule a revelação de todas as células não bombas
-    for i in range(8):
-        for j in range(8):
-            if campo_minado.tabuleiro[i][j] != -1:
-                campo_minado.botoes[i][j]['state'] = 'disabled'
-
-    # Verifique se a função retorna True, indicando que o jogador venceu
-    assert campo_minado.verificar_vitoria() == True
-    
-    
-    
-def test_game_over_no_interface():
-    # Criar uma instância de CampoMinado com dimensões 8x8 e 1 bomba
-    root = None
-    linhas = 8
-    colunas = 8
-    num_bombas = 1
-    campo_minado = CampoMinado(root, linhas, colunas, num_bombas)
-
-    # Colocar uma bomba em uma célula (0, 0)
-    campo_minado.tabuleiro[0][0] = -1
-
-    # Chamar a função game_over sem exibir a interface
-    campo_minado.game_over(mostrar_interface=False)
-
-    # Verificar se os botões foram desativados e marcados com 'X'
-    assert campo_minado.botoes[0][0]['text'] == 'X'
-    assert campo_minado.botoes[0][0]['state'] == 'disabled'
-    
-
-          
+            
 if __name__ == "__main__":
     pytest.main()
 

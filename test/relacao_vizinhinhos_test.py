9c1a34bd893faeae867b03339a42190edce9be25
Matheus Andrade Uchoa <uchoamatheus22@gmail.com>
2023-10-07 19:51:43 -0300

    usando paratrize pra organizar melhor o codigo

--- test/relacao_vizinhinhos_test.py
+++ test/relacao_vizinhinhos_test.py
@@ -52,8 +52,16 @@ def test_calcular_vizinhos_canto_inferior__esquerdo_com_bombas():
     vizinhos = campo_minado.calcular_vizinhos(7,0)  
     assert vizinhos == 2
     
-def test_calcular_vizinhos_canto_inferior_direito_com_bombas():
-    bomb_positions = [(7, 1), (6, 0)]  
+#modo Dificil
+@pytest.mark.parametrize("posicao, bomb_positions, expected_vizinhos", [
+    ((0, 0), [(0, 1), (1, 0), (1, 1)], 3),  
+    ((0, 23), [(0, 22), (1, 22), (1, 23)], 3),  
+    ((23, 0), [(22, 0), (22, 1), (23, 1)], 3), 
+    ((23, 23), [(22, 22), (22, 23), (23, 22)], 3),  
+    ((12, 12), [(11, 11), (11, 12), (11, 13), (12, 11), (12, 13), (13, 11), (13, 12), (13, 13)], 8)  #não tenho ideia de onde ta mas deve ta perto do centro(muito grande)
+])
+def test_calcular_vizinhos_dificil_com_bombas(posicao, bomb_positions, expected_vizinhos):
+    campo_minado = CampoMinado(None, 24, 24, 0)
     posicionar_bombas(campo_minado, bomb_positions)
     vizinhos = campo_minado.calcular_vizinhos(7,0)  
     assert vizinhos == 2

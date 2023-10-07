1db1e37bf971615e5a4428d3415d2c0cbd73c4a9
Matheus Andrade Uchoa <uchoamatheus22@gmail.com>
2023-10-07 16:16:07 -0300

    relação de vizinhança no canto superitor esquerdo

--- test/relacao_vizinhinhos_test.py
+++ test/relacao_vizinhinhos_test.py
@@ -11,6 +11,14 @@ def test_calcular_vizinhos_canto_superior_esquerdo_sem_bombas():
     vizinhos = campo_minado.calcular_vizinhos(0,0)  
     assert vizinhos == 0
 
+def test_calcular_vizinhos_canto_superior_esquerdo_com_bombas():
+    campo_minado = CampoMinado(None, 8, 8, 0)  
+    bomb_positions = [(0, 1), (1, 0), (1, 1)] 
+    posicionar_bombas(campo_minado, bomb_positions)
+    vizinhos = campo_minado.calcular_vizinhos(0, 0)
+    assert vizinhos == 3 
+
+
 
     
 

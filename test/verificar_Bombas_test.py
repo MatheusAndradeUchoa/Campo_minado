0741291951d0388e354e841e2e00672730ad45f3
Matheus Andrade Uchoa <uchoamatheus22@gmail.com>
2023-10-08 19:48:14 -0300

    refatorando toda as verificaçoes de bomba

--- test/verificar_Bombas_test.py
+++ test/verificar_Bombas_test.py
@@ -48,7 +48,7 @@ def test_posicoes_bombas(posicao_bombas):
 
     for x, y in posicao_bombas:
         campo_minado.tabuleiro[x][y] = -1
-        
+    
     for x, y in posicao_bombas:
         assert campo_minado.tabuleiro[x][y] == -1
         
@@ -79,65 +79,56 @@ def test_posicoes_bombas__modo_intermediario(posicao_bomba):
     [(4, 6), (6, 2), (3, 1), (2, 7)],
     [(5, 1), (0, 2), (4, 3), (6, 4)]
     ])      
-def test_calcular_vizinhos_intermediario_sem_bomba(posicao, expected_vizinhos):
-    campo_minado_intermediario = CampoMinado(None, 10, 16, 0) 
-    vizinhos = campo_minado_intermediario.calcular_vizinhos(*posicao)
-    assert vizinhos == expected_vizinhos
+def test_posicoes_bombas__modo_dificil(posicao_bombas):
+    campo_minado = CampoMinado(None, 24, 24, 4)
 
-#modo Dificil
-@pytest.mark.parametrize("posicao, expected_vizinhos",[
-    ((0, 0),0),  
-    ((0, 23),0),  
-    ((23, 0), 0), 
-    ((23, 23), 0),  
-    ((12, 12),0)  #não tenho ideia de onde ta mas deve ta perto do centro(muito grande)
-])
-def test_calcular_vizinhos_dificil_sem_bomba(posicao, expected_vizinhos):
-    campo_minado = CampoMinado(None, 24, 24, 0)
-    vizinhos = campo_minado.calcular_vizinhos(*posicao)
-    assert vizinhos == expected_vizinhos
-
-#Test para calcular vizinhos nos cantos e no centro do tabuleiro com BOMBA
-#Modo Facil
-@pytest.mark.parametrize("posicao, bomb_positions, expected_vizinhos", [
-    ((0, 0), [(0, 1), (1, 0), (1, 1)], 3),  # Canto superior esquerdo
-    ((0, 7), [(0, 6), (1, 6), (1, 7)], 3),  # Canto superior direito
-    ((7, 0), [(6, 0), (6, 1), (7, 1)], 3),  # Canto inferior esquerdo
-    ((7, 7), [(6, 6), (6, 7), (7, 6)], 3),  # Canto inferior direito
-    ((4, 4), [(3, 3), (3, 4), (3, 5), (4, 3), (4, 5), (5, 3), (5, 4), (5, 5)], 8)  # algum lugar no centro
-])
-def test_calcular_vizinhos_facil_com_bombas(posicao, bomb_positions, expected_vizinhos): 
-    posicionar_bombas(campo_minado, bomb_positions)
-    vizinhos = campo_minado.calcular_vizinhos(*posicao)
-    assert vizinhos == expected_vizinhos
-
-#Modo Intermediario
-@pytest.mark.parametrize("posicao, bomb_positions, expected_vizinhos", [
-    ((0, 0), [(0, 1), (1, 0), (1, 1)], 3),  # Canto superior esquerdo
-    ((0, 15), [(0, 14), (1, 14), (1, 15)], 3),  # Canto superior direito
-    ((9, 0), [(8, 0), (8, 1), (9, 1)], 3),  # Canto inferior esquerdo
-    ((9, 15), [(8, 14), (8, 15), (9, 14)], 3),  # Canto inferior direito
-    ((5, 7), [(4, 6), (4, 7), (4, 8), (5, 6), (5, 8), (6, 6), (6, 7), (6, 8)], 8) #não tenho ideia de onde ta mas deve ta perto do centro(muito grande)
-])
-def test_calcular_vizinhos_intermediario_com_bombas(posicao, bomb_positions, expected_vizinhos):
-    campo_minado = CampoMinado(None, 10, 16, 0)
-    posicionar_bombas(campo_minado, bomb_positions)
-    vizinhos = campo_minado.calcular_vizinhos(*posicao)
-    assert vizinhos == expected_vizinhos
+    for x, y in posicao_bombas:
+        campo_minado.tabuleiro[x][y] = -1
     
-#modo Dificil
-@pytest.mark.parametrize("posicao, bomb_positions, expected_vizinhos", [
-    ((0, 0), [(0, 1), (1, 0), (1, 1)], 3),  
-    ((0, 23), [(0, 22), (1, 22), (1, 23)], 3),  
-    ((23, 0), [(22, 0), (22, 1), (23, 1)], 3), 
-    ((23, 23), [(22, 22), (22, 23), (23, 22)], 3),  
-    ((12, 12), [(11, 11), (11, 12), (11, 13), (12, 11), (12, 13), (13, 11), (13, 12), (13, 13)], 8)  #não tenho ideia de onde ta mas deve ta perto do centro(muito grande)
-])
-def test_calcular_vizinhos_dificil_com_bombas(posicao, bomb_positions, expected_vizinhos):
-    campo_minado = CampoMinado(None, 24, 24, 0)
-    posicionar_bombas(campo_minado, bomb_positions)
-    vizinhos = campo_minado.calcular_vizinhos(*posicao)
-    assert vizinhos == expected_vizinhos
+    for x, y in posicao_bombas:
+        assert campo_minado.tabuleiro[x][y] == -1
+
+
+# Teste para posições inválidas
+@pytest.mark.parametrize("posicao_invalida", [
+    [(-1, 0),], 
+    [(0, -1),], 
+    [(8, 0),], 
+    [(0, 8)], 
+    [(5, 10)], [(15, 3)], [(10, -5)], [(13, 20)],[(-1,-1)],[(8,8)]]
+)
+def test_posicionar_bombas_posicao_invalida_modo_facil(posicao_invalida):
+    with pytest.raises(ValueError):
+        campo_minado = CampoMinado(None, 8, 8, 10)
+        campo_minado.posicionar_bombas_em_posicoes(posicao_invalida)
+
+
+@pytest.mark.parametrize("posicao_invalida", [
+    [(-1, 0),], 
+    [(0, -1),], 
+    [(11, 0),], 
+    [(10, 17)], 
+    [(31, 10)], [(31, 3)], [(10, -5)], [(13, 20)],[(-1,-1)],[(11,17)]]
+)
+def test_posicionar_bombas_posicao_invalida_modo_intermediario(posicao_invalida):
+    with pytest.raises(ValueError):
+        campo_minado = CampoMinado(None, 10, 16, 10)
+        campo_minado.posicionar_bombas_em_posicoes(posicao_invalida)
+
+@pytest.mark.parametrize("posicao_invalida", [
+    [(-1, 0)], 
+    [(0, -1)], 
+    [(11, -1)], 
+    [(-1, 25)], 
+    [(31, 10)], [(31, 3)], [(10, -5)], [(25, 20)], [(-1,-1)],[(25,25)]]
+)
+def test_posicionar_bombas_posicao_invalida_modo_dificil(posicao_invalida):
+    with pytest.raises(ValueError):
+        campo_minado = CampoMinado(None, 24,24, 10)
+        campo_minado.posicionar_bombas_em_posicoes(posicao_invalida)
+
+
+
 
 
 

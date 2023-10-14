d25378fb2bb511cb3c2600a894aa9678c6a84b46
Matheus Andrade Uchoa <uchoamatheus22@gmail.com>
2023-10-14 14:15:11 -0300

    verifica celular de relaçao de sem bombas

--- test/revelar_celula_test.py
+++ test/revelar_celula_test.py
@@ -12,6 +12,22 @@ def test_revelar_celula():
     
     vizinhos_esperados = campo_minado.calcular_vizinhos(0, 2)  
     assert campo_minado.botoes[0][2]['text'] == str(vizinhos_esperados) 
+def test_revelar_celula_sem_bombas_adjacentes():
+    #root = tkinter.Tk()
+    campo_minado = CampoMinado(None, 8, 8, 10)
+
+    
+    campo_minado.revelar_adjacentes = MagicMock()
+
+    for i in range(campo_minado.linhas):
+        for j in range(campo_minado.colunas):
+            if (i, j) != (1, 1):
+                campo_minado.tabuleiro[i][j] = 0
+
+    campo_minado.revelar_celula(1, 1)
+    campo_minado.revelar_adjacentes.assert_called_with(1, 1)
+    
+       
 
 
 

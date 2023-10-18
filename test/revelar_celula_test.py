3b693a9520b00386905e31aa5864d9375f4204d3
Matheus Andrade Uchoa <uchoamatheus22@gmail.com>
2023-10-18 10:56:11 -0300

    alteraçoes nos teste de revelar cellula

--- test/revelar_celula_test.py
+++ test/revelar_celula_test.py
@@ -4,15 +4,7 @@ from unittest.mock import MagicMock, patch
 
 from src.campo_minado import CampoMinado
 
-def test_revelar_celula():
-    campo_minado = CampoMinado(None, 8, 8, 10)
-    
-   # botoes = [['disabled'] * campo_minado.colunas for _ in range(campo_minado.linhas)]
-    
-    campo_minado.revelar_celula(0, 2)  
-    
-    vizinhos_esperados = campo_minado.calcular_vizinhos(0, 2)  
-    assert campo_minado.botoes[0][2]['text'] == str(vizinhos_esperados) 
+
 def test_revelar_celula_sem_bombas_adjacentes():
     #root = tkinter.Tk()
     campo_minado = CampoMinado(None, 8, 8, 10)
@@ -50,8 +42,13 @@ def test_revelar_celula_chama_game_over():
         campo_minado.revelar_celula(0, 0)
 
         mock_game_over.assert_called_once()
-    
-       
+
+
+@pytest.fixture
+def campo_minado():
+    # Suponha que você tenha uma instância de CampoMinado
+    campo_minado = CampoMinado(None, 8, 8, 10)
+    return campo_minado
 
 
 

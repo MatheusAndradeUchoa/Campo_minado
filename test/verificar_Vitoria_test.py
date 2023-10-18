3682ab8e5f99b32f4c28685dc6cfb6d70c916d31
Matheus Andrade Uchoa <uchoamatheus22@gmail.com>
2023-10-18 10:57:19 -0300

    adicionando parametrize nos teste de vitoria

--- test/verificar_Vitoria_test.py
+++ test/verificar_Vitoria_test.py
@@ -17,3 +17,26 @@ def test_verificar_vitoria():
                 campo_minado.botoes[i][j]['state'] = 'disabled'
 
     assert campo_minado.verificar_vitoria() == True
+
+def test_verificar_vitoria_intermediario():
+    campo_minado = CampoMinado(None, 10, 16, 30)
+
+  
+    for i in range(10):
+        for j in range(16):
+            if campo_minado.tabuleiro[i][j] != -1:
+                campo_minado.botoes[i][j]['state'] = 'disabled'
+
+    assert campo_minado.verificar_vitoria() == True
+
+
+def test_verificar_vitoria_dificil():
+    campo_minado = CampoMinado(None, 24, 24, 100)
+
+  
+    for i in range(24):
+        for j in range(24):
+            if campo_minado.tabuleiro[i][j] != -1:
+                campo_minado.botoes[i][j]['state'] = 'disabled'
+
+    assert campo_minado.verificar_vitoria() == True
